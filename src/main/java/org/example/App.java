34291c44cd83c3c6d91b2aea5295cf4f3b201b0b
M Usama Alvi <m.usamaalvi@gmail.com>
2022-02-28 22:40:41 +0500

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+import io.vertx.core.Vertx;
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Vertx vertx = Vertx.vertx();
+        System.out.println( "Hello World!" );
+    }
+}
