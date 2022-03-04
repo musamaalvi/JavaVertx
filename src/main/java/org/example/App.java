77885a30dd4271fc2ed91dc1304a285c43265432
M Usama Alvi <m.usamaalvi@gmail.com>
2022-03-04 14:23:55 +0000

    Adding simple handler to requests.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
 /**
  * Hello world!
  *
@@ -11,6 +15,21 @@ public class App
     public static void main( String[] args )
     {
         Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+
+        router
+                .route()
+                .handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response.putHeader("content-type", "text/plain");
+                    response.end("Hi Alvi");
+                });
+
+
+        httpServer
+                .requestHandler(router)
+                .listen(8091);
         System.out.println( "Hello World!" );
     }
 }
